583de48867771409c998d89e89d8c71013742836
Raul <you@example.com>
2024-01-22 12:30:38 +0100

    add async to update & delete methods and customizes response

--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Repository, FindManyOptions } from 'typeorm';
 import { Book } from './book.entity';
 import { BookDto } from './book.dto';
 
@@ -10,33 +10,35 @@ export class BookService {
     @InjectRepository(Book)
     private bookRepository: Repository<Book>,
   ) {}
-  findAll(limit: string): Promise<Book[]> {
-    let options: {};
+  async findAll(limit: string): Promise<Book[]> {
+    let options: FindManyOptions<Book>;
     if (limit) options = { take: +limit };
     return this.bookRepository.find(options);
     //return `findAll funciona límite de ${limit} registros`;
   }
-  findBook(bookId: string): Promise<Book> {
+  async findBook(bookId: string): Promise<Book> {
     return this.bookRepository.findOneBy({ id: +bookId });
     //return `findBook funciona con el bookId = ${bookId}`;
     //select * from book where id = bookId
   }
-  createBook(book: BookDto) {
+  async createBook(book: BookDto): Promise<Book> {
     // console.log(book);
     //insert into table Book values();
     // return book;
     return this.bookRepository.save(book);
   }
-  updateBook(bookId: string, book: BookDto) {
+  async updateBook(bookId: string, book: BookDto) {
     // console.log('update', book);
 
     // return `updateBook funciona con el bookId = ${bookId}`;
     //select * from book where id = bookId
-    return this.bookRepository.update({ id: +bookId }, book);
+    const result = await this.bookRepository.update({ id: +bookId }, book);
+    if (result) return { message: 'updated OK' };
   }
-  deleteBook(bookId: string) {
+  async deleteBook(bookId: string) {
     // return `deleteBook funciona con el bookId = ${bookId}`;
-    return this.bookRepository.delete({ id: +bookId });
+    const result = await this.bookRepository.delete({ id: +bookId });
+    if (result) return { message: 'deleted OK' };
     //select * from book where id = bookId
   }
 }

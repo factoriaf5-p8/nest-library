99969e4805424cb2073b576596c230a41c797a06
Raul <you@example.com>
2024-01-26 22:36:07 +0100

    add transaction control to loanService.borrowBook()

--- src/loan/loan.service.ts
+++ src/loan/loan.service.ts
@@ -1,7 +1,7 @@
 import { HttpException, Injectable } from '@nestjs/common';
 import { CreateLoanDto } from './dto/create-loan.dto';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { InjectDataSource, InjectRepository } from '@nestjs/typeorm';
+import { DataSource, Repository } from 'typeorm';
 import { Loan } from './entities/loan.entity';
 import { Book } from '../book/book.entity';
 
@@ -12,20 +12,50 @@ export class LoanService {
     private loanRepository: Repository<Loan>,
     @InjectRepository(Book)
     private bookRepository: Repository<Book>,
+    @InjectDataSource()
+    private dataSource: DataSource,
   ) {}
   async borrowBook(createLoanDto: CreateLoanDto) {
     //TO-DO: lógica de decisión si el libro está disponible
     //update available: false
 
-    const availableBook = await this.bookRepository.findOne({
-      where: { id: createLoanDto.book.id, available: true },
-    });
-    console.log(availableBook);
+    // const availableBook = await this.bookRepository.findOne({
+    //   where: { id: createLoanDto.book.id, available: true },
+    // });
+    // console.log(availableBook);
 
-    if (availableBook) {
-      return this.loanRepository.save(createLoanDto);
-    } else {
-      throw new HttpException(`book not available`, 404);
+    // if (availableBook) {
+    //   return this.loanRepository.save(createLoanDto);
+    // } else {
+    //   throw new HttpException(`book not available`, 404);
+    // }
+    const queryRunner = this.dataSource.createQueryRunner();
+
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+
+    try {
+      const availableBook = await queryRunner.manager.findOne(Book, {
+        where: { id: createLoanDto.book.id, available: true },
+      });
+
+      if (!availableBook) {
+        throw new HttpException(`book not available`, 404);
+      }
+
+      const loan = await queryRunner.manager.save(Loan, createLoanDto);
+
+      availableBook.available = false;
+      await queryRunner.manager.save(Book, availableBook);
+
+      await queryRunner.commitTransaction();
+
+      return loan;
+    } catch (error) {
+      await queryRunner.rollbackTransaction();
+      throw error;
+    } finally {
+      await queryRunner.release();
     }
   }
   async findAll(): Promise<Loan[]> {
